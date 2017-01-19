b31cec449c5be65c91ea9dc71c4f2fb3757d877e
Jhesed Tacadena (Ben/TAS/Other <jhesed.tacadena@towerswatson.com>
2017-01-19 09:53:22 +0800

    Deleted directories irrelevant to jsoscc

--- riaevasco/php_exam_jhesed/db_config.php
+++
@@ -1,8 +0,0 @@
-<?php
-    define('DB_HOST', 'mysql1.000webhost.com');
-    define('DB_USER', 'a3176676_pmm');
-	define('DB_PASSWORD', '12345jhesed');
-    define('DB_DATABASE', 'a3176676_pmm');
-    $conn=mysql_connect(DB_HOST, DB_USER, DB_PASSWORD) or die ("Error connecting to " . DB_DATABASE);
-    mysql_select_db(DB_DATABASE) or die ("Error: Cannot access " . DB_DATABASE . " database");
-?>
